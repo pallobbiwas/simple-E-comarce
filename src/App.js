e9c47c76757e0e435d283f98bdbcf91d413f8f37
pallobbiswas <pallobbiswas326@gmail.com>
2022-03-22 12:19:55 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="text-center">
       <Header></Header>
@@ -12,4 +19,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,13 @@
-import React from "react";
 import("./Product.css");
+
 
 const Product = ({ product, addToCart }) => {
   const { name, img, seller, ratings, price } = product;
+
   return (
-    <div className="product-container">
+    <div className="product-container"  data-aos="fade-down"
+    data-aos-easing="linear"
+    data-aos-duration="1500">
       <div className="mx-3">
         <img className="w-100 img-fluid p-4 rounded-3" src={img} alt="" />
         <div className="details p-4">
@@ -15,7 +18,12 @@ const Product = ({ product, addToCart }) => {
           <p>saller: {seller}</p>
         </div>
       </div>
-      <button onClick={() => addToCart(product)} className="footer w-75 my-4 py-2">Add to cart</button>
+      <button
+        onClick={() => addToCart(product)}
+        className="footer w-75 my-4 py-2"
+      >
+        Add to cart
+      </button>
     </div>
   );
 };
